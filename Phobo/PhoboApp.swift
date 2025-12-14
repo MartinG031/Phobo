993cffa14ab8bfd9be1b9f9a6df1e223dfbe944c
MartinG031 <93334469+MartinG031@users.noreply.github.com>
2025-12-14 18:01:52 +0800

    Initial Commit

---
+++ Phobo/PhoboApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoboApp.swift
+//  Phobo
+//
+//  Created by Martin Jay on 2025/12/14.
+//
+
+import SwiftUI
+
+@main
+struct PhoboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
